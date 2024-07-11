2332be8254b3fc91a1980eab262004200c446317
jose.neto <jose.neto@infracommerce.com.br>
2024-07-11 19:07:10 -0300

    feat: create fetch api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <>Teste</>;
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch("https://api.github.com/users/octocat", {
+        mode: "no-cors",
+      });
+
+      if (!response.ok) {
+        console.log(response);
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
